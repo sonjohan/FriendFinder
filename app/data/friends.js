118e9eca8716efe72ce221fd9935cbf01b9437cf
JohanBermeo <sonjohan@hotmail.com>
2018-10-12 23:37:18 -0400

    completed modal needs some more work

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var friends = [
+    { 
+        "name": "Johan", 
+        "photo": "http://vignette3.wikia.nocookie.net/vsbattles/images/7/72/Kid-gohan.png/revision/latest?cb=20150411184446", 
+        "scores": [
+            "3", 
+            "4", 
+            "5", 
+            "4", 
+            "5", 
+            "4", 
+            "2", 
+            "3", 
+            "2", 
+            "1"
+        ] 
+    }
+];
+
+module.exports = friends;
